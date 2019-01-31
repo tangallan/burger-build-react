0f65234a385ea876ad134e9f299c5d587eb2d214
Allan Tang <tang.allan03@gmail.com>
2019-01-31 09:15:47 -0800

    Init commit + started on burger build app

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from './Burger.module.css';
+
+const burger = props => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            // [,]
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient
+                    key={igKey + 1}
+                    type={igKey}
+                />
+            });
+        })
+        .reduce((arr, i) => {
+            return arr.concat(i);
+        });
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
+
+    return <div className={classes.Burger}>
+            {/* <BurgerIngredient type='bread-top' />
+            <BurgerIngredient type='meat' />
+            <BurgerIngredient type='cheese' />
+            <BurgerIngredient type='bread-bottom' /> */}
+            <BurgerIngredient type='bread-top' />
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom' />
+        </div>;
+};
+
+export default burger;
