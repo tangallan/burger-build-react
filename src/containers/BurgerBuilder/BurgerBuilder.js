5746be5fb881fdca5d251d24e7836a21c3f1e62f
Allan Tang <tang.allan03@gmail.com>
2019-02-14 09:11:18 -0800

    Changed purchaseable to use internal function

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -34,7 +34,6 @@ class BurgerBuilder extends Component {
     }
 
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -48,9 +47,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({
-            purchasable: sum > 0
-        });
+        return sum > 0;
     }
 
     // MOVED TO REDUX ;) 
@@ -132,7 +129,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                     />
                 </>
