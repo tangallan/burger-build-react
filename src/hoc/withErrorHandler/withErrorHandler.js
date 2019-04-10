0302a492076d72f5e6c34506f59c0d11a6200c2b
Allan Tang <tang.allan03@gmail.com>
2019-04-09 21:34:35 -0700

    Moved withErrorHandler to use hooks

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,46 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 
 const withErrorHandler = (WrappedComponent, axios) => {
-    return class extends Component {
-        state = {
-            error: null
-        };
+    return props => {
+    // return class extends Component {
+        // state = {
+        //     error: null
+        // };
+        const [error, setError] = useState(null);
 
-        componentWillMount() {
-            this.reqInterceptor = axios.interceptors.request.use(req => {
-                this.setState({ error: null });
-                return req;
-            });
-            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
-                this.setState({ error: error });
-            });
-        }
+        // componentWillMount() {
+        //     this.reqInterceptor = axios.interceptors.request.use(req => {
+        //         this.setState({ error: null });
+        //         return req;
+        //     });
+        //     this.resInterceptor = axios.interceptors.response.use(res => res, error => {
+        //         this.setState({ error: error });
+        //     });
+        // }
+        const reqInterceptor = axios.interceptors.request.use(req => {
+            // this.setState({ error: null });
+            setError(null);
+            return req;
+        });
+        
+        const resInterceptor = axios.interceptors.response.use(res => res, err => {
+            // this.setState({ error: error });
+            setError(err);
+        });
 
-        componentWillUnmount() {
-            axios.interceptors.request.eject(this.reqInterceptor);
-            axios.interceptors.response.eject(this.resInterceptor);
-        }
+        // componentWillUnmount() {
+        //     axios.interceptors.request.eject(this.reqInterceptor);
+        //     axios.interceptors.response.eject(this.resInterceptor);
+        // }
+        useEffect(() => {
+            return () => {
+                axios.interceptors.request.eject(reqInterceptor);
+                axios.interceptors.response.eject(resInterceptor);
+            };
+        }, [reqInterceptor, resInterceptor]); // if we return a () it will run during unmount
+        // cleans up when these ^^^ changes
         
 
-        errorDismissHandler = () => {
-            this.setState({ error: null });
+        const errorDismissHandler = () => {
+            // this.setState({ error: null });
+            setError(null);
         }
 
-        render() {
-            return (
-                <>
-                    <Modal
-                        show={this.state.error}
-                        modalClosed={this.errorDismissHandler}>
-                        {this.state.error
-                            ? this.state.error.message
-                            : `Something didn't go as plan! Try again!`}
-                    </Modal>
-                    <WrappedComponent {...this.props} />
-                </>
-            );
-        }
+        // render() {
+        return (
+            <>
+                <Modal
+                    show={error}
+                    modalClosed={errorDismissHandler}>
+                    {error ? error.message : `Something didn't go as plan! Try again!`}
+                </Modal>
+                <WrappedComponent {...props} />
+            </>
+        );
+        // }
     };
 };
 
